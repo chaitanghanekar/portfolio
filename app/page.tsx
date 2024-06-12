12d448c8b17efbd364b269f7f2d15d695091db7a
chaitanghanekar <60208418+chaitanghanekar@users.noreply.github.com>
2024-06-12 14:06:01 -0700

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Welcome
       </h1>
-      <p>Hello! I'm Chaitan Ghanekar, a dedicated and enthusiastic developer based in Kelowna, Canada. At 27, I have a strong educational background, having graduated from the University of British Columbia with a focus on Computer Science, Mathematics, and Data Science. My passion for technology and problem-solving drives me to continuously learn and take on new challenges.</p>
+      <p>Hello! I'm Chaitan Ghanekar, a dedicated and enthusiastic developer based in Kelowna, Canada. I have a strong educational background, having graduated from the University of British Columbia with a focus on Computer Science, Mathematics, and Data Science. My passion for technology and problem-solving drives me to continuously learn and take on new challenges.</p>
     </section>
     </>
   );
