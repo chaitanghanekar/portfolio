03906b630ed8a7e5b5cd281c11d1e85a6bf7bd91
chaitanghanekar <60208418+chaitanghanekar@users.noreply.github.com>
2024-06-12 11:49:16 -0700

    customize

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,3 +1,6 @@
+import IconContacts from '../icons/IconContacts';
+import IconGithub from '../icons/IconGithub';
+
 function ArrowIcon() {
   return (
     <svg
@@ -24,10 +27,10 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="/rss"
+            href="https://www.linkedin.com/in/chaitan/"
           >
-            <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
+            <IconContacts width={24} height={24} fill="currentColor" />
+            <p className="ml-2 h-7">connect</p>
           </a>
         </li>
         <li>
@@ -37,25 +40,11 @@ export default function Footer() {
             target="_blank"
             href="https://github.com/vercel/next.js"
           >
-            <ArrowIcon />
+            <IconGithub width={24} height={24} fill="currentColor"/>
             <p className="ml-2 h-7">github</p>
           </a>
         </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
-          </a>
-        </li>
       </ul>
-      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
-      </p>
     </footer>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,15 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Welcome
       </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
+      <p>Hello! I'm Chaitan Ghanekar, a dedicated and enthusiastic developer based in Kelowna, Canada. At 27, I have a strong educational background, having graduated from the University of British Columbia with a focus on Computer Science, Mathematics, and Data Science. My passion for technology and problem-solving drives me to continuously learn and take on new challenges.</p>
+      <br></br>
+        <ul>
+          <li>Programming Languages: MATLAB, Java, C, SQL</li>
+          <li>Web Development: HTML, CSS, JavaScript, PHP, Bootstrap, Next.js, React</li>
+          <li>Tools & Technologies: Jira, Git, Android Development</li>
+        </ul>
     </section>
   )
 }
