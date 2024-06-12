58b61a18d4229238baacc3d088946038c630d9e1
chaitanghanekar <60208418+chaitanghanekar@users.noreply.github.com>
2024-06-12 11:55:55 -0700

    yes

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Chaitan Ghanekar Portfolio',
+    template: '%s | Chaitan Ghanekar Portfolio',
   },
-  description: 'This is my portfolio.',
+  description: 'Portfolio.',
   openGraph: {
     title: 'My Portfolio',
     description: 'This is my portfolio.',
