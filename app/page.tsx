1fd918e36d93e397b3eb7b7e8cb8167cd08c056c
chaitanghanekar <60208418+chaitanghanekar@users.noreply.github.com>
2024-06-12 13:38:14 -0700

    icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
-import { BlogPosts } from 'app/components/posts'
+import Head from 'next/head';
 
 export default function Page() {
   return (
+    <>
+    <Head>
+      <link rel="icon" href="/favicon.ico"/>
+    </Head>
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Welcome
       </h1>
       <p>Hello! I'm Chaitan Ghanekar, a dedicated and enthusiastic developer based in Kelowna, Canada. At 27, I have a strong educational background, having graduated from the University of British Columbia with a focus on Computer Science, Mathematics, and Data Science. My passion for technology and problem-solving drives me to continuously learn and take on new challenges.</p>
     </section>
-  )
+    </>
+  );
 }
