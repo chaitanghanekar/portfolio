f76f1cec9e1dad86b0e5ebc9b89551f222ec6304
chaitanghanekar <60208418+chaitanghanekar@users.noreply.github.com>
2024-07-09 21:01:08 -0700

    link clarity on project page

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -1,6 +1,23 @@
 import Link from 'next/link'
 import { formatDate, getBlogPosts } from 'app/projects/utils'
 
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
+
 export function BlogPosts() {
   let allBlogs = getBlogPosts()
 
@@ -25,9 +42,12 @@ export function BlogPosts() {
               <p className="text-sm mt-0.5 text-neutral-400 w-[120px] tabular-nums ">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
-              <p className="tracking-tight hover-secondary">
-                {post.metadata.title}
-              </p>
+              <div className="flex items-center space-x-1 hover-secondary">
+                <p className="tracking-tight">
+                  {post.metadata.title}
+                </p>
+                <ArrowIcon />
+              </div>
             </div>
           </Link>
         ))}
